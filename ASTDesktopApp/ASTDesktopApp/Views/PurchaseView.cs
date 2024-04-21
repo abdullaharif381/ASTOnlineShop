2fba91d4ba828aa704205338d8481673cdebfd4b
Abdullah Arif <arifabdullah764@gmail.com>
2024-04-21 09:55:17 +0500

    Added a comment in purchase view

--- ASTDesktopApp/ASTDesktopApp/Views/PurchaseView.cs
+++ ASTDesktopApp/ASTDesktopApp/Views/PurchaseView.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace ASTDesktopApp.Views
 {
+    // this is purchase view
     public partial class PurchaseView : SampleViews
     {
         public PurchaseView()
